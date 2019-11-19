6d07f38937aa0a935c3fbd5cc4bef693d895a1a7
Karina Mendes Correia <ka.mendes.correia@gmail.com>
2019-11-18 23:35:53 -0300

    O conceito e como declarar char e String

---
+++ 5-CaracteresETextos/5-CaracteresETextos/Praticando char e String/Praticando char e String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praticando_char_e_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mão na massa: Praticando char e String");
+
+            char letra = 'a';
+            Console.WriteLine(letra);
+
+            //tabela ascii
+            char valor = (char)65;
+            Console.WriteLine(valor);      // Compila!
+
+
+            valor = (char)(valor + 1);   // complila! 
+            Console.WriteLine(valor);
+
+            string palavra = "alura cursos online de tecnologia";
+            Console.WriteLine(palavra);
+
+
+            palavra = palavra +  2020;
+            Console.WriteLine(palavra);
+
+            Console.ReadLine();
+
+        }
+    }
+}
